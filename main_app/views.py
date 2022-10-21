13b155b96412e7d8f8d673e9e248af5c463d21c9
Joey Fasanelli <joeyfas1704@gmail.com>
2022-10-21 12:21:36 -0400

    added home route and bootstrap styling to base html

--- main_app/views.py
+++ main_app/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('')
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
