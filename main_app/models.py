bcc881feb324d5e86b89039a71e3ae154bffd210
Joey Fasanelli <joeyfas1704@gmail.com>
2022-10-21 14:29:22 -0400

    added Movie class in views and have a movie card that pops up on the movies page

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,2 @@
 from django.db import models
 
-class Movie:
-    def __init__(self, title, genre, year, director, description, poster):
-        self.title = title
-        self.genre = genre
-        self.year = year
-        self.director = director
-        self.description = description
-        self.poster = poster
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 # Define the home view
 def home(request):
     return render(request, 'home.html')
@@ -9,5 +7,18 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def movie_index(request):
+def movies_index(request):
     return render(request, 'movies/index.html', {'movies': movies})
+
+class Movie:
+    def __init__(self, title, genre, year, director, description, poster):
+        self.title = title
+        self.genre = genre
+        self.year = year
+        self.director = director
+        self.description = description
+        self.poster = poster
+
+movies = [
+    Movie('Bladerunner', 'Sci-Fi', '1982', 'Ridley Scott', 'In a cyberpunk vision of the future, man has developed the technology to create replicants - humanoid androids with short, fixed lifespans - which are illegal on Earth, but are used in the off-world colonies.', 'no image')
+]
