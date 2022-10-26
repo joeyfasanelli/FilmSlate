17e1c906e12419b71e5353eee110b34c167fe929
Joey Fasanelli <joeyfas1704@gmail.com>
2022-10-25 20:22:48 -0400

    added favicon

--- filmslate/settings.py
+++ filmslate/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    str(BASE_DIR / 'main_app' / 'static'),
+]
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
